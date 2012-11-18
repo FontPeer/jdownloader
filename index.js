0e9f2108c97bb6131cdbf25ac43c86e27cc2c847
Tomas Fejfar <tomas.fejfar@gmail.com>
2012-11-18 15:30:02 +0100

    test

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-var http    = require('http');
+#!/bin/env node
+//  OpenShift sample Node application
+var http = require('http');
 
-// Get the environment variables we need.
-var ipaddr = process.env.OPENSHIFT_INTERNAL_IP;
-var port = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+//Get the environment variables we need.
+var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
+var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
-http.createServer(function(req,res) {
+http.createServer(function (req, res) {
+    var addr = "unknown";
+    var out = "";
+    if (req.headers.hasOwnProperty('x-forwarded-for')) {
+        addr = req.headers['x-forwarded-for'];
+    } else if (req.headers.hasOwnProperty('remote-addr')){
+        addr = req.headers['remote-addr'];
+    }
 
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('hello Tom Peeters from Nodejs\n');
+    if (req.headers.hasOwnProperty('accept')) {
+        if (req.headers['accept'].toLowerCase() == "application/json") {
+              res.writeHead(200, {'Content-Type': 'application/json'});
+              res.end(JSON.stringify({'ip': addr}, null, 4) + "\n");
+              return ;
+        }
+    }
 
-}).listen(port,ipaddr);
-
-console.log("server running at http://" + ipaddr + ":" + port + "/");
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write("Welcome to Node.js on OpenShift!\n\n");
+  res.end("Your IP address seems to be " + addr + "\n");
+}).listen(port, ipaddr);
+console.log("Server running at http://" + ipaddr + ":" + port + "/");
